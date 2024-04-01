0d1a910b3f00f43325da0d07a21b7002f1e9601e
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-04-01 22:01:38 +0700

    Add All Block Printer

--- src/main/java/com/khopan/blockimage/BlockImage.java
+++ src/main/java/com/khopan/blockimage/BlockImage.java
@@ -1,9 +1,15 @@
 package com.khopan.blockimage;
 
+import java.util.Iterator;
+import java.util.Map.Entry;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod(BlockImage.MOD_ID)
 public class BlockImage {
@@ -14,5 +20,16 @@ public class BlockImage {
 
 	public BlockImage() {
 		BlockImage.LOGGER.info("Initializing {}", BlockImage.MOD_NAME);
+		BlockImage.printAllBlocks();
+	}
+
+	private static void printAllBlocks() {
+		Iterator<Entry<ResourceKey<Block>, Block>> iterator = ForgeRegistries.BLOCKS.getEntries().iterator();
+
+		while(iterator.hasNext()) {
+			Entry<ResourceKey<Block>, Block> entry = iterator.next();
+			ResourceKey<Block> key = entry.getKey();
+			System.out.println(key.toString());
+		}
 	}
 }
